5a0f766918b80b50f0135310f14c86b22e3a144a
Matthew Ma <mtthman@amazon.com>
2021-06-08 18:31:03 -0700

    Add event listener to RawMessageDelete

--- bot.py
+++ bot.py
@@ -12,6 +12,10 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 @client.event
+async def on_ready():
+	print("bot ready")
+
+@client.event
 async def on_message(message):
 	if message.author == client.user:
 		return
@@ -19,9 +23,19 @@ async def on_message(message):
 	match = re.match(r"https://discordapp\.com/channels/(\d+)/(\d+)/(\d+)", message.content)
 	if match:
 		guild_id, channel_id, message_id = match[1], match[2], match[3]
+		print("detected cross channel reference")
 		ref_channel = await client.fetch_channel(channel_id)
 		ref_msg = await ref_channel.fetch_message(message_id)
 		msg = "<@!%s> you have been summoned by <@!%s> " % (ref_msg.author.id, message.author.id)
 		await message.channel.send(msg)
+
+@client.event
+async def on_raw_message_delete(message):
+	print("detected raw message delete")
+	print(message)
+	if message.cached_message:
+		id = message.cached_message.author.id
+		user = await client.fetch_user(id)
+		await user.send("beep boop: your message in the <#%s> channel was deleted" % message.channel_id)
 
 client.run(TOKEN)
