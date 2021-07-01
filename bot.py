5ad7837ebee095232a53e9d5a4179d6fa39949d7
EC2 Default User <ec2-user@ip-172-31-46-14.us-east-2.compute.internal>
2021-07-01 23:20:39 +0000

    Update cross channel reference to exclude self-referencing

--- bot.py
+++ bot.py
@@ -28,8 +28,9 @@ async def on_message(message):
 		print("detected cross channel reference")
 		ref_channel = await client.fetch_channel(channel_id)
 		ref_msg = await ref_channel.fetch_message(message_id)
-		msg = "<@!%s> you have been summoned by <@!%s> " % (ref_msg.author.id, message.author.id)
-		await message.channel.send(msg)
+                if ref_msg.author.id != message.author.id:
+	    	    msg = "<@!%s> you have been summoned by <@!%s> " % (ref_msg.author.id, message.author.id)
+		    await message.channel.send(msg)
 
 	match = re.match(r".*theory.*", message.content.lower())
 	if match:
