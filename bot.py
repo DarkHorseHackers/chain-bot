11c3361ac2c54c8ae22fe9518ec6999ebb9e4f49
Matthew Ma <mtthman@amazon.com>
2021-07-17 11:01:33 -0700

    Add channel name change command

--- bot.py
+++ bot.py
@@ -43,6 +43,14 @@ async def on_message(message):
 		channel = await client.fetch_channel(message.channel.id)
 		await generate_wordcloud_for_channel(channel)
 
+	match = re.match(r"!name (.*)", message.content.lower())
+	if match:
+		name = match[1]
+		print("detected channel name change")
+		msg = "changing channel name <#%s> -> %s" % (message.channel.id, name)
+		await message.channel.send(msg)
+		await message.channel.edit(name=name)
+
 async def generate_wordcloud_for_channel(channel):	
 	messages = []
 
