002712dc03d9ec2b5d1f185e4718d9aa3424ade2
Matthew Ma <mtthman@amazon.com>
2021-08-15 23:37:25 -0700

    Create new episode discussion text channel when MonitoRSS posts new livestream episode

--- bot.py
+++ bot.py
@@ -31,6 +31,18 @@ async def on_message(message):
 	if message.author == client.user:
 		return
 
+	if message.author.id == 268478587651358721: # check if MonitoRSS posted a new livestream episode
+		match = re.match(r".*Bret and Heather (.*) DarkHorse Podcast Livestream.*", message.content)
+		if match:
+			live_stream = match[1]
+			live_stream_number = int("".join(e for e in live_stream if e.isnumeric()))
+			episode_name = "episode-%s" % live_stream_number
+			channel = discord.utils.get(client.get_all_channels(), name=episode_name)
+			category = await client.fetch_channel(800076391156547634) # category: Episode Discussions
+			if not channel:
+				new_channel = await message.guild.create_text_channel(episode_name, category=category)
+				print("CREATE CHANNEL: ", new_channel)
+
 	match = re.match(r"https://discord(app)?\.com/channels/(\d+)/(\d+)/(\d+)", message.content)
 	if match:
 		guild_id, channel_id, message_id = match[2], match[3], match[4]
