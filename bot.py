684debce485743bc039693b3022072cc72e9bf5b
Matthew Ma <mtthman@amazon.com>
2022-01-10 23:46:16 -0800

    Add hints to filtering mechanism

--- bot.py
+++ bot.py
@@ -25,41 +25,53 @@ async def on_ready():
 
 reference_referrer_pairs = [(755811907948118179, 741321254027526195), (726866762523738202, 743540914336694392)]
 
+QUESTION_SUBMISSION_LIMIT = 50
+bot_sandbox_channel_id = 730163671191519342
+question_submission_channel_id = 755811907948118179
+question_chat_channel_id = 741321254027526195
+
 @client.event
 async def on_message(message):
 	print(message.content, message.author)
 	if message.author == client.user:
 		return
-		
+	
+	# NOTE: to install discord.py 2.0, use 
+	# pip install git+https://github.com/Rapptz/discord.py
 	if any(role.name == 'Organizer' or role.name == 'Moderator' for role in message.author.roles): # whitelist moderator and organizer roles
 		pass
 	elif message.type == discord.MessageType.thread_created: # don't kill system messages (which actually contain user content) created upon thread creation
 		pass
 	else:
 		print(message.type)
-		# rules: [rule lambda, message to send violator]
-		contains_hyperlink = [lambda m: re.match(r".*https://.*", m, re.S), "contains hyperlink"]
-		content_too_long = [lambda m: len(m.split(" ")) > 1, "content too long"]
-		does_not_contain_bold_text = [lambda m: not re.match(r".*\*\*.*\*\*.*", m, re.S), "does not contain bold text"]
-		does_not_contain_hyperlink = [lambda m: not re.match(r".*https://.*", m, re.S), "does not contain hyperlink"]
+		# rules: [rule lambda, message to send violator, hint]
+		contains_hyperlink = [lambda m: re.match(r".*https://.*", m, re.S), "contains hyperlink", "links can be shared in <#%s>" % question_chat_channel_id]
+		content_too_long = [lambda m: len(m.split(" ")) > QUESTION_SUBMISSION_LIMIT, "content too long", ""]
+		does_not_contain_bold_text = [lambda m: not re.match(r".*\*\*.*\*\*.*", m, re.S), "does not contain bold text", ""]
+		does_not_contain_hyperlink = [lambda m: not re.match(r".*https://.*", m, re.S), "does not contain hyperlink", ""]
 
 		# [list of rule, channel id using these rules]
 		all_rules = [
-			[[contains_hyperlink, content_too_long], 730163671191519342],
-			[[does_not_contain_hyperlink], 730163671191519342]
+			[[contains_hyperlink, content_too_long], question_submission_channel_id],
 		]
 
 		did_violate = False
 		violations = []
+		hints = []
 		for rules, rule_channel_id in all_rules:
 			if message.channel.id == rule_channel_id:
-				for rule_lambda, violation_message in rules:
+				for rule_lambda, violation_message, hint in rules:
 					if rule_lambda(message.content):
 						violations.append(violation_message)
+						hints.append(hint)
 						did_violate = True
 		if did_violate:
 			await message.delete()
-			await message.author.send("Booooop! Your message in <#%s> was deleted for the following violations:\n" % message.channel.id + "\n".join("**"+v+"**" for v in violations))
+			await message.author.send(
+				"Booooop! Your message in <#%s> was deleted for the following violations:\n\n" % message.channel.id + 
+				"\n".join(v + "\n" + h for v, h in zip(("**"+v+"**" for v in violations), ("*"+h+"*" if h else "" for h in hints))) +
+				"\n\nPlease resubmit your entry!"
+			)
 
 	if message.author.id == 268478587651358721: # check if MonitoRSS posted a new livestream episode
 		match = re.match(r".*Bret and Heather (.*) DarkHorse Podcast Livestream.*(https\:\/\/odysee\.com\/.*)", message.content, re.S)
