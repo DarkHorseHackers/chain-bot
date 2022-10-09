8558fcaba399c020c4d28d866304a8f8c0ece092
PlasmaIntec <alphaintec@gmail.com>
2022-10-09 14:03:54 -0700

    Use argparse to replace meme face and text

--- bot.py
+++ bot.py
@@ -14,12 +14,17 @@ import discord
 from dotenv import load_dotenv
 from generate_wordcloud import generate_wordcloud
 from generate_meme import generate_meme
+import requests
+import argparse
+import shlex
+import shutil
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guilds = True
 client = discord.Client(intents=intents)
 
 @client.event
@@ -35,6 +40,8 @@ question_chat_channel_id = 741321254027526195
 
 @client.event
 async def on_message(message):
+	if message.author.id != 744749168903585854:
+		return
 	print(message.content, message.author)
 	if message.author == client.user:
 		return
@@ -146,7 +153,52 @@ async def on_message(message):
 	if match:
 		text = match[1]
 		print("detected couple, %s" % text)
-		generate_meme(text)
+
+		parser = argparse.ArgumentParser(description='Process meme arguments.')
+		parser.add_argument('--man_profile_id', type=int)
+		parser.add_argument('--woman_profile_id', type=int)
+		parser.add_argument('--man_text')
+		parser.add_argument('--woman_text')
+		parser.add_argument('text', nargs='?', default="")
+		args = vars(parser.parse_args(shlex.split(text)))
+		print("couple args: ", args)
+		
+		DEFAULT_TEXT0 = "i bet he's thinking about other women"
+		CIN_URL = "https://i.ibb.co/P1NGNqz/cin.png"
+		man_profile_id = args["man_profile_id"]
+		woman_profile_id = args["woman_profile_id"]
+		text = args["text"]
+		man_text = args["man_text"] if args["man_text"] else text
+		woman_text = args["woman_text"] if args["woman_text"] else DEFAULT_TEXT0
+	
+		if man_profile_id:
+			# special cin case
+			if man_profile_id == 456226577798135808:
+				img_url = CIN_URL
+			else:
+				profile = await client.fetch_user(man_profile_id)
+				img_url = profile.avatar.url.split("?")[0] + "?size=80"
+			print(img_url)
+			img_data = requests.get(img_url).content
+
+			with open('man-profile.jpg', 'wb') as handler:
+				handler.write(img_data)
+
+		if woman_profile_id:
+			# special cin case
+			if woman_profile_id == 456226577798135808:
+				img_url = CIN_URL
+			else:
+				profile = await client.fetch_user(woman_profile_id)
+				img_url = profile.avatar.url.split("?")[0] + "?size=80"
+			print(img_url)
+			img_data = requests.get(img_url).content
+
+			with open('woman-profile.jpg', 'wb') as handler:
+				handler.write(img_data)
+
+		generate_meme(text0=woman_text, text1=man_text, replace_man_profile=True if man_profile_id else False, replace_woman_profile=True if woman_profile_id else False)
+
 		with open('meme.jpg', 'rb') as fp:
 			await message.channel.send(file=discord.File(fp, 'meme.jpg'))
 
--- generate_meme.py
+++ generate_meme.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from PIL import Image
 
 load_dotenv()
 
@@ -10,22 +11,35 @@ url = "https://api.imgflip.com/caption_image"
 imgflip_username = os.getenv('IMGFLIP_USERNAME')
 imgflip_password = os.getenv('IMGFLIP_PASSWORD')
 
-payload = 'template_id=130331290&username=' + imgflip_username + '&password=' + imgflip_password + '&text0=i%20bet%20he\'s%20thinking%20about%20other%20women'
+payload = 'template_id=130331290&username=' + imgflip_username + '&password=' + imgflip_password
 headers = {
   'Content-Type': 'application/x-www-form-urlencoded'
 }
 
-def generate_meme(text: str):
+def generate_meme(text1: str, text0: str, replace_man_profile: bool = False, replace_woman_profile: bool = False):
     try:
-        data = payload + '&text1=' + text
+        data = payload + '&text1=' + text1 + '&text0=' + text0
         response = requests.request("POST", url, headers=headers, data=data)
         response_json = json.loads(response.text)
         img_url = response_json["data"]["url"]
 
-        print(img_url)
+        print("meme url: ", img_url)
 
         img_data = requests.get(img_url).content
         with open('meme.jpg', 'wb') as handler:
-            handler.write(img_data)
+            handler.write(img_data)    
+
+        background = Image.open("meme.jpg")
+
+        if replace_man_profile:
+            man_profile = Image.open("man-profile.jpg")
+            background.paste(man_profile, (650, 130), man_profile.convert('RGBA'))
+
+        if replace_woman_profile:
+            woman_profile = Image.open("woman-profile.jpg")
+            background.paste(woman_profile, (230, 80), woman_profile.convert('RGBA'))
+
+        background.save('meme.jpg')
+
     except Exception as e:
         print("Error: ", e)
