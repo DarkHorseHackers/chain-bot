6e968d09c62cffc5f45c53dc760080d730af990b
PlasmaIntec <alphaintec@gmail.com>
2022-10-22 16:24:47 -0700

    Update ApplicationStop hook to check exit status

--- scripts/stop_bot.sh
+++ scripts/stop_bot.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 pkill -9 -f bot.py
-exit 0
+pkillexitstatus=$?
+
+if [ $pkillexitstatus -eq 0 ]; then
+    echo "pkill: one or more processes killed"
+elif [ $pkillexitstatus -eq 1]; then
+    echo "pkill: no processes killed"
+elif [ $pkillexitstatus -eq 2]; then
+    echo "pkill: syntax error in the command line"
+elif [ $pkillexitstatus -eq 3]; then
+    echo "pkill: fatal error"
+else
+    echo UNEXPECTED
+fi
