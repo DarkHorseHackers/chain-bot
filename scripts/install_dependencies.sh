aceb93864a12c357857fc3cb5139a24c07250163
PlasmaIntec <alphaintec@gmail.com>
2022-10-22 17:33:10 -0700

    Remove bad dependency

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,4 +2,4 @@
 sudo yum install python3-pip
 python3.8 -m venv /home/ec2-user/chain-bot/venv
 source /home/ec2-user/chain-bot/venv
-yes | pip3 install -r /home/ec2-user/chain-bot/requirements.txt
+yes | sudo pip3 install -r /home/ec2-user/chain-bot/requirements.txt
