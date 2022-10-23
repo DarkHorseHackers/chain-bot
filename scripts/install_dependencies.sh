6342f44b453508372c39f9d8dbc2afb3c5250018
PlasmaIntec <alphaintec@gmail.com>
2022-10-22 17:57:57 -0700

    Properly use virtualenv

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum install python3-pip
 python3.8 -m venv /home/ec2-user/chain-bot/venv --system-site-packages
-source /home/ec2-user/chain-bot/venv
+source /home/ec2-user/chain-bot/venv/bin/activate
 pip install -r /home/ec2-user/chain-bot/requirements.txt --user
 pip install -U discord.py --user
