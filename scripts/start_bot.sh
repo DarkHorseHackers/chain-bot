71cfa74a8464d788aec716b90cccfa790bf44378
PlasmaIntec <alphaintec@gmail.com>
2022-10-22 23:18:38 -0700

    Send input to /dev/null

--- scripts/start_bot.sh
+++ scripts/start_bot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ec2-user/chain-bot/venv/bin/activate
 chmod 777 /tmp/chain-bot
-python /home/ec2-user/chain-bot/bot.py > /tmp/chain-bot 2>&1 &
+python /home/ec2-user/chain-bot/bot.py > /tmp/chain-bot 2>&1 < /dev/null &
