07654543c1b16f31af91e3fe9cca06d2261e0f3c
PlasmaIntec <alphaintec@gmail.com>
2022-10-22 18:16:44 -0700

    Chown step

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo yum install python3-pip
 python3.8 -m venv /home/ec2-user/chain-bot/venv --system-site-packages
+chown ec2-user:ec2-user /home/ec2-user/chain-bot/venv
+chown ec2-user:ec2-user /home/ec2-user/chain-bot/venv/*
 source /home/ec2-user/chain-bot/venv/bin/activate
 pip install -r /home/ec2-user/chain-bot/requirements.txt --user
 pip install -Iv discord.py==2.0.0 --user
