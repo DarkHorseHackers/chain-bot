086c4bec0ae5523025d2f1154162439398b125a9
PlasmaIntec <alphaintec@gmail.com>
2022-10-22 17:27:18 -0700

    Use venv

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo yum install python3-pip
+python3.8 -m venv /home/ec2-user/chain-bot/venv
+source /home/ec2-user/chain-bot/venv
 yes | pip3 install -r /home/ec2-user/chain-bot/requirements.txt
