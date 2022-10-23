3133c80caf65d704c47ec4214d924ceb66e99704
PlasmaIntec <alphaintec@gmail.com>
2022-10-22 18:05:13 -0700

    Ignore installed packages

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,4 +3,4 @@ sudo yum install python3-pip
 python3.8 -m venv /home/ec2-user/chain-bot/venv --system-site-packages
 source /home/ec2-user/chain-bot/venv/bin/activate
 pip install -r /home/ec2-user/chain-bot/requirements.txt --user
-pip install discord.py==2.0.0 --user
+pip install -Iv discord.py==2.0.0 --user
