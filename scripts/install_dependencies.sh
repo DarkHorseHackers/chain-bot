a8010f764361a4cbed32f1880463302a9be57453
PlasmaIntec <alphaintec@gmail.com>
2022-10-22 17:57:12 -0700

    Set --system-site-packages

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo yum install python3-pip
-python3.8 -m venv /home/ec2-user/chain-bot/venv
+python3.8 -m venv /home/ec2-user/chain-bot/venv --system-site-packages
 source /home/ec2-user/chain-bot/venv
-yes | sudo pip3 install -r /home/ec2-user/chain-bot/requirements.txt
+pip install -r /home/ec2-user/chain-bot/requirements.txt --user
+pip install -U discord.py --user
