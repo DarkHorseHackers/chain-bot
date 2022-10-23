248c6b17c6195cab9fe54f7fb722547fb8f73d8b
PlasmaIntec <alphaintec@gmail.com>
2022-10-22 22:48:37 -0700

    Redirect all output to stdout

--- scripts/start_bot.sh
+++ scripts/start_bot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/ec2-user/chain-bot/venv/bin/activate
-python /home/ec2-user/chain-bot/bot.py > /dev/null 2> /dev/null < /dev/null &
+python /home/ec2-user/chain-bot/bot.py 2>&1 &
