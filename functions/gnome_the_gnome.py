1ce0b0fdad1e5379c54c7fe081f70f91f415b2c8
PlasmaIntec <alphaintec@gmail.com>
2022-11-06 11:47:33 -0800

    React to all gnome posts with up and down gnome emojis

--- functions/gnome_the_gnome.py
+++ functions/gnome_the_gnome.py
@@ -1,8 +1,10 @@
 from discord import Message
 
 GNOME_ID = 326183956485767171
-GNOME_EMOJI = "<:DH_Gnome:1012067067945566209>"
+GNOME_UP_EMOJI = "<:DH_Gnome:1012067067945566209>"
+GNOME_DOWN_EMOJI = "<:DH_Gnome:1038305325679386685>"
 
 async def gnome_the_gnome(message: Message):
 	if message.author.id == GNOME_ID:
-		await message.add_reaction(GNOME_EMOJI)
+		await message.add_reaction(GNOME_UP_EMOJI)
+		await message.add_reaction(GNOME_DOWN_EMOJI)
