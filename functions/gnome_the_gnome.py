6a0a18ca4f060b84620945a50264c6375bd21dc5
PlasmaIntec <alphaintec@gmail.com>
2022-11-06 23:18:03 -0800

    Fix emoji names

--- functions/gnome_the_gnome.py
+++ functions/gnome_the_gnome.py
@@ -1,8 +1,8 @@
 from discord import Message
 
 GNOME_ID = 326183956485767171
-GNOME_UP_EMOJI = "<:DH_Gnome:1012067067945566209>"
-GNOME_DOWN_EMOJI = "<:DH_Gnome:1038305325679386685>"
+GNOME_UP_EMOJI = "<:DH_GnomeUp:1012067067945566209>"
+GNOME_DOWN_EMOJI = "<:DH_GnomeDown:1038305325679386685>"
 
 async def gnome_the_gnome(message: Message):
 	if message.author.id == GNOME_ID:
