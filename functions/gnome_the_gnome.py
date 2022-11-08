2344adcc49ebbe6d67d109b54ebe861d7ad9b58d
PlasmaIntec <alphaintec@gmail.com>
2022-11-07 17:12:11 -0800

    Fix emoji id again

--- functions/gnome_the_gnome.py
+++ functions/gnome_the_gnome.py
@@ -2,7 +2,7 @@ from discord import Message
 
 GNOME_ID = 326183956485767171
 GNOME_UP_EMOJI = "<:DH_GnomeUp:1012067067945566209>"
-GNOME_DOWN_EMOJI = "<:DH_GnomeDown:1037963548413919263>"
+GNOME_DOWN_EMOJI = "<:DH_GnomeDown:1038145968299835452>"
 
 async def gnome_the_gnome(message: Message):
 	if message.author.id == GNOME_ID:
