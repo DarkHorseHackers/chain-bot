7b2700bb1b40ba87ee72ddd3652072dce1ba77ac
Matthew Ma <matthew.ma@wallawalla.edu>
2022-11-27 12:06:44 -0800

    Fix spacing in detect_twitter

--- functions/detect_twitter.py
+++ functions/detect_twitter.py
@@ -2,10 +2,10 @@ import re
 from discord import Message
 
 async def detect_twitter(message: Message):    
-	notmatch = re.match(r".*fxtwitter.com.*", message.content.lower())
-	if !notmatch:
-    match = re.match(r".*twitter.com.*", message.content.lower())
+	notmatch = re.match(r".*fxtwitter\.com.*", message.content.lower())
+	if not notmatch:
+		match = re.match(r".*twitter\.com.*", message.content.lower())
 		if match:
-      message.edit(content = message.content.replace("twitter", "fxtwitter"))
+			message.edit(content = message.content.replace("twitter", "fxtwitter"))
       
       
