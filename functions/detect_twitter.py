370e2724abe3361f8278f98246bbb363d82f81ed
LanguidSquid <blakecameronyarbrough@gmail.com>
2023-01-13 10:34:03 -0700

    updating twitter function to send new message rather than editing existing message

--- functions/detect_twitter.py
+++ functions/detect_twitter.py
@@ -1,11 +1,10 @@
 import re
 from discord import Message
 
-async def detect_twitter(message: Message):    
+async def detect_twitter(message: Message):
 	notmatch = re.match(r".*fxtwitter\.com.*", message.content.lower())
 	if not notmatch:
 		match = re.match(r".*twitter\.com.*", message.content.lower())
 		if match:
-			message.edit(content = message.content.replace("twitter", "fxtwitter"))
-      
-      
+			msg = message.content.replace("twitter", "fxtwitter")
+			await message.channel.send(msg)
